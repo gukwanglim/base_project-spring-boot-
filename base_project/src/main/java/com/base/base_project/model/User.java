728443781340268d0e2cc3757d8528cfdbbd08fc
gukwanglim <100823895+gukwanglim@users.noreply.github.com>
2022-09-30 01:58:17 +0900

    user 테이블 생성

---
+++ base_project/src/main/java/com/base/base_project/model/User.java
@@ -0,0 +1,44 @@
+package com.base.base_project.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column
+    private String role;
+
+    @CreatedDate
+    private Timestamp createdDate;
+
+}
