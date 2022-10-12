9d0c63558df7668e0a857e1947e5ad18781cbd01
gukwanglim <100823895+gukwanglim@users.noreply.github.com>
2022-10-13 04:46:17 +0900

    list html 파일 post mapping

--- base_project/src/main/java/com/base/base_project/repositroy/ProjectRepositroy.java
+++ base_project/src/main/java/com/base/base_project/repositroy/ProjectRepositroy.java
@@ -7,6 +7,7 @@ import com.base.base_project.model.Project;
 
 public interface ProjectRepositroy extends JpaRepository<Project, Integer>{
     
-    @Query(value="INSERT INTO reply(userNo, userNm, userId, userPw) VALUES(?1, ?2, ?3, ?4);", nativeQuery = true)
+    // QueryDSL 사용해보기(현재 사용하는 건 QueryDSL 아님)
+    @Query(value="INSERT INTO project(userNm, userId, userPw) VALUES(?1, ?2, ?3);", nativeQuery = true)
 	void mSave(String userNm, String userId, String userPw);
 }
--- base_project/src/main/java/com/base/base_project/service/ProjectService.java
+++ base_project/src/main/java/com/base/base_project/service/ProjectService.java
@@ -2,6 +2,7 @@ package com.base.base_project.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,10 @@ import lombok.RequiredArgsConstructor;
 public class ProjectService {
 
     // @RequiredArgsConstructor와 final을 사용하여 @Autowired를 대체
-    private final ProjectRepositroy projectRepositroy;
+    // private final ProjectRepositroy projectRepositroy;
+
+    @Autowired
+    private ProjectRepositroy projectRepositroy;
 
     public List<Project> find() {    // Project 테이블에 들어있는 모든 내용을 findAll()을 사용하여 가져오는 것은 list의 형태로 들고 오게 된다.
         return projectRepositroy.findAll();
@@ -25,6 +29,7 @@ public class ProjectService {
 
     @Transactional
     public void 저장(ProjectDto projectDto) {
+        // projectRepositroy.save(null);
         projectRepositroy.mSave(projectDto.getUserNm(), projectDto.getUserId(), projectDto.getUserPw());
     }
     
